e8074fd9fa10e5249381e515e590079b6e55f227
Hanna Christensson <145654643+HannaChristensson@users.noreply.github.com>
2024-01-17 16:48:55 +0100

    Initial Commit

---
+++ SwiftExempel/SwiftExempelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExempelApp.swift
+//  SwiftExempel
+//
+//  Created by Hanna Christensson on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExempelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
