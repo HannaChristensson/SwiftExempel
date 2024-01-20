c53249e764743895bb5fbea7c6ce2857dbfe6e47
Hanna Christensson <145654643+HannaChristensson@users.noreply.github.com>
2024-01-20 21:35:44 +0100

    Ny uppdatering med Playground

---
+++ SwiftExempel/Exempel UI/ButtonTestView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonTestView.swift
+//  SwiftExempel
+//
+//  Created by Hanna Christensson on 2024-01-18.
+//
+
+import SwiftUI
+
+struct ButtonTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonTestView()
+}
---
+++ SwiftExempel/Exempel UI/ViewTestView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewTestView.swift
+//  SwiftExempel
+//
+//  Created by Hanna Christensson on 2024-01-17.
+//
+
+import SwiftUI
+
+struct ViewTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewTestView()
+}
